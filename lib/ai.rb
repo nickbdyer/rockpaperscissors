e34abd3f091c12375e620e504f7b5878081b981c
Nick Dyer <nbdyer@gmail.com>
2014-11-15 21:25:48 +0000

    Ai can make random selection

--- lib/ai.rb
+++ lib/ai.rb
@@ -1,14 +1,14 @@
 class Ai
 
   attr_reader :options
+  attr_reader :selection
 
   def initialize(*args)
     @options = args.map{|arg| arg.new }
   end
 
-
-
   def make_selection
+    @selection = options.sample
   end
 
 end
