5cc2fae42b152c50317476d6f586698c866638b7
Nick Dyer <nbdyer@gmail.com>
2014-11-16 09:24:28 +0000

    Paper knows what it beats

--- spec/paper_spec.rb
+++ spec/paper_spec.rb
@@ -2,9 +2,15 @@ require 'paper'
 
 describe "Paper" do
 
+  let(:paper) { Paper.new    }
+  let(:rock)  { double :rock, :class => Rock }
+
   it "should have the class Paper" do
-    paper = Paper.new
     expect(paper.class).to eq Paper
+  end
+
+  it "should know if it is a winning selecton" do
+    expect(paper.won?(rock)).to be true
   end
 
 end
