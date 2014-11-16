5b310e5a977e4bbdba2e8245fe8ed5eed4152b86
Nick Dyer <nbdyer@gmail.com>
2014-11-16 09:27:51 +0000

    Rock knows what class it beats

--- lib/rock.rb
+++ lib/rock.rb
@@ -1,2 +1,7 @@
 class Rock
+
+  def won?(opponent_selection)
+    opponent_selection.class == Scissors
+  end
+
 end
--- spec/rock_spec.rb
+++ spec/rock_spec.rb
@@ -2,9 +2,17 @@ require 'rock'
 
 describe "Rock" do
 
+  let(:rock) { Rock.new    }
+  let(:scissors)  { double :scissors, :class => Scissors }
+
   it "should have the class Rock" do
-    rock = Rock.new
     expect(rock.class).to eq Rock
   end
+
+
+  it "should know what class it beats" do
+    expect(rock.won?(scissors)).to be true
+  end
+
 
 end
